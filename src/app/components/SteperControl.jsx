dbd9c268b10388788ce57da284edc70937edf2be
DilanGerber <130708466+DilanGerber@users.noreply.github.com>
2024-08-14 01:59:16 -0300

    añadi el PaymentButton para una logica independiente cuando se llega al step 3

--- src/app/components/SteperControl.jsx
+++ src/app/components/SteperControl.jsx
@@ -1,6 +1,7 @@
 "use client"
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { StepperContext } from '@/contexts/StepperContext';
+import PaymentButton from './uiPayment/PaymentButton';
 
 const SteperControl = ({ steps }) => {
     const { state, dispatch } = useContext(StepperContext);
@@ -35,7 +36,7 @@ const SteperControl = ({ steps }) => {
         <div className='container flex justify-around mt-2 mb-4 sm:mt-4 sm:mb-8'>
             <button
                 onClick={handleBackClick}
-                className={`bg-white text-slate-400 uppercase py-1.5 sm:py-2 px-4 rounded-lg font-semibold border-2 border-slate-300 transition duration-200 ease-in-out text-xs sm:text-base ${
+                className={`bg-white text-slate-400 uppercase py-1.5 sm:py-2 px-4 rounded-lg font-semibold border-2 border-slate-300 transition duration-200 ease-in-out text-xs sm:text-base shadow-md ${
                     currentStep === 1 ? "  opacity-50 cursor-not-allowed" : "hover:bg-slate-700 hover:text-white cursor-pointer"
                 }`}
                 disabled={currentStep === 1}
@@ -43,17 +44,41 @@ const SteperControl = ({ steps }) => {
                 Volver
             </button>
 
-            <button
-                onClick={handleNextClick}
-                className={`bg-red-500 text-white uppercase py-1.5 sm:py-2 px-4 rounded-lg font-semibold transition duration-200 ease-in-out text-xs sm:text-base ${
-                    isNextDisabled() ? "opacity-50 cursor-not-allowed" : "hover:bg-red-700 hover:text-white cursor-pointer"
-                }`}
-                disabled={isNextDisabled()}
-            >
-                {currentStep === steps.length ? "Confirmar" : "Siguiente"}
-            </button>
+            {currentStep === steps.length ? ( <PaymentButton isNextDisabled={isNextDisabled} /> ) : (
+                <button
+                    onClick={handleNextClick}
+                    className={`bg-red-600 text-white uppercase py-1.5 sm:py-2 px-4 rounded-lg font-semibold transition duration-200 ease-in-out text-xs sm:text-base shadow-md ${
+                        isNextDisabled() ? "opacity-50 cursor-not-allowed" : "hover:bg-red-700 hover:text-white cursor-pointer"
+                    }`}
+                    disabled={isNextDisabled()}
+                >
+                    Siguiente
+                </button>
+            )}
+
         </div>
     );
 };
 
 export default SteperControl;
+
+
+// (
+//     <button 
+    // className={`flex items-center py-1.5 sm:py-2 px-4 rounded-lg font-semibold transition duration-200 ease-in-out text-xs sm:text-base shadow-md ${
+    //     isLoading ? 'bg-gray-500 text-gray-300 hover:bg-gray-500 cursor-not-allowed' 
+    //     : 'bg-red-600 text-white '
+    // } ${isNextDisabled() ? "opacity-50 cursor-not-allowed" : " hover:bg-red-700 "}`}
+//     onClick={handlerClickPayment}
+//     disabled={isLoading || isNextDisabled() }
+// >
+//     {isLoading ? (
+//         <>
+//             <span>Cargando</span> 
+//             <SpinnerIcon className="animate-spin h-5 w-5 ml-3" />
+//         </>
+//     ) : (
+//         'Confirmar'
+//     )}
+// </button>
+// )
--- src/app/components/steps/Payment.jsx
+++ src/app/components/steps/Payment.jsx
@@ -17,7 +17,7 @@ const Payment = () => {
         // Verifica si todos los campos del formulario están completos
         const isComplete = Object.values(updatedData).every(field => field !== '');
         if (isComplete) {
-            dispatch({ type: 'COMPLETE_STEP', step: 'form' });
+            dispatch({ type: 'COMPLETE_STEP', step: 'payment' });
         }
     };
 
