42eb0368ebfc8fd3b16320312dda2a047005a819
DilanGerber <130708466+DilanGerber@users.noreply.github.com>
2025-05-08 22:48:28 -0300

    weas

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,11 @@
-
-import App from "./components/AtsDatePiker";
+import Hero from "@/components/views/Hero";
 import Modal from "../components/detailUI/Modal";
-import Slider from "./components/Slider";
-import SliderMio from "./components/SliderMio";
-
 
 export default function Home() {
   return (
-    <main className="">
-      {/* <Slider/> */}
-      <SliderMio />
-      <Modal />
+    <main className=" ">
+      {/* <Modal /> */}
+      <Hero />
     </main>
   );
 }
---
+++ src/components/views/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className=' h-screen grid-cols-2 grid max-w-[1200px] mx-auto gap-8 my-20'>
+        <div className=' flex bg-slate-500 w-full h-full'>
+            wea
+        </div>
+        <div className=' flex bg-slate-500 w-full h-max-[200px]'>
+            wea2
+        </div>
+        <div className=' bg-slate-500 w-full h-full'>
+            wea3
+        </div>
+        <div className=' bg-slate-500 w-full h-max-[200px]'>
+            wea4
+        </div>
+    </div>
+  )
+}
+
+export default Hero
